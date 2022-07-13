59739a6644d775cfe14c0d3e909594c232e08a72
Archana <a.rchana0486@gmail.com>
2022-07-13 10:53:20 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	//public static void main(String[] args) {
+public static void main(String[] args) {
+	Test tobj=new Test();
+	tobj.add();
+}
+public void add()
+{
+	System.out.println("this is add function");
+	}
+}
+
+
