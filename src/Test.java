05a79e7e7b112b7dfd1acf303a8f32ce82139022
Archana <a.rchana0486@gmail.com>
2022-07-13 12:44:45 +0530

    American guy commited

--- src/Test.java
+++ src/Test.java
@@ -11,6 +11,12 @@ public void add()
 	System.out.println("this is add2 function");
 	System.out.println("this is add3 function");
 	System.out.println("this is add4 function");
+	//ftfhg
+	//hfh
+	System.out.println("this is add1 function1");
+	System.out.println("this is add2 function2");
+	System.out.println("this is add3 function3");
+	System.out.println("this is add4 function4");
 	}
 }
 
